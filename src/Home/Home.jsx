7a066a7c5ef03e46ed4bf618f9df90afee4e7f03
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-08-19 15:56:01 +0530

    looks better now

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div className=" flex flex-col w-screen h-screen justify-center items-center bg-[#84d36f] text-[black] ">
-      Go to somewhere else, this place ain't so fun.
+      Go to some other place, this place ain't so fun.
     </div>
   );
 }
