1498566308c174eaecb3ac75848772b00bea638d
Abhinay-Katta <abhinaykatta97@gmail.com>
2023-08-20 16:58:56 +0530

    feat:made it more bootiful

--- src/WeatherApp/Weather.jsx
+++ src/WeatherApp/Weather.jsx
@@ -41,38 +41,57 @@ function Weather() {
     console.log("Unable to retrieve your location");
   }
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen bg-slate-800 text-teal-600">
-      <h1 className="md:text-5xl lg:text-6xl text-4xl text-center font-extrabold top-[10%]  absolute">
-        Whatever Weather
-      </h1>
-      <div className="flex flex-row justify-center items-center top-0  border-2 border-[#435342]">
-        {weather && (
-          <div className="w-auto flex flex-row mx-4">
-            <p className="md:text-5xl text-4xl lg:text-6xl font-bold mx-3">
-              {weather.current.temp_c}
-            </p>
-            <p className="text-sm flex justify-center">
-              <sup className="font-bold">O</sup>
-            </p>
-            <p className="md:text-5xl text-4xl lg:text-6xl font-bold">C</p>
-            <img
-              className="w-[20%] h-[20%]"
-              src={weather.current.condition.icon}
-              alt="current weather icon"></img>
-          </div>
-        )}
-        {weather && location.length !== 0 && (
-          <div className=" flex flex-col">
-            <p className=" h-auto font-bold md:text-5xl lg:text-6xl text-4xl  p-1">
-              {weather && weather.location.name}
-            </p>
-            <p className=" h-auto md:text-5xl text-4xl lg:text-6xl p-1">
-              {weather.location.region}
-            </p>
-            <p className=" h-auto text-2xl p-1">{weather.location.country}</p>
-            <p className=" h-auto text-2xl p-1">{weather.location.localtime}</p>
-          </div>
-        )}
+    <div className="flex flex-col justify-center items-center w-screen  h-screen bg-cover sm:bg-[url(D:\Projects\React\learningreact\src\WeatherApp\assets\mobilebackground.jpg)] md:bg-[url(D:\Projects\React\learningreact\src\WeatherApp\assets\pcbackground.jpg)] bg-[url(D:\Projects\React\learningreact\src\WeatherApp\assets\mobilebackground.jpg)] xs:bg-[url(D:\Projects\React\learningreact\src\WeatherApp\assets\pcbackground.jpg)] text-teal-900 md:text-white">
+      <div className="flex flex-col justify-center items-center w-screen h-screen backdrop-blur-lg absolute "></div>
+      <div className="flex flex-col justify-center items-center w-screen h-screen    absolute">
+        <h1 className="md:text-5xl lg:text-6xl text-4xl text-center font-extrabold top-[10%]  absolute">
+          Whatever Weather
+        </h1>
+        <div className="flex flex-row  justify-center items-center top-0 rounded-[2rem] p-3 border-2 border-[#435342]">
+          {weather && (
+            <div className="w-auto animate-pulse flex flex-row mx-4">
+              <p className="md:text-5xl  text-4xl lg:text-6xl font-bold mx-3">
+                {weather.current.temp_c}
+              </p>
+              <p className="text-sm flex justify-center">
+                <sup className="font-bold text-sm">o</sup>
+              </p>
+              <p className="md:text-5xl text-4xl lg:text-6xl font-bold">C</p>
+              <div className="  w-[20%] h-[20%]">
+                <img
+                  className=""
+                  src={weather.current.condition.icon}
+                  alt="current weather icon"></img>
+              </div>
+            </div>
+          )}
+          {weather && location.length !== 0 && (
+            <div className=" flex flex-col">
+              <p className=" h-auto font-bold md:text-5xl lg:text-6xl text-4xl  p-1">
+                {weather && weather.location.name}
+              </p>
+              <p className=" h-auto md:text-5xl text-4xl lg:text-6xl p-1">
+                {weather.location.region}
+              </p>
+              <p className=" h-auto text-2xl p-1">{weather.location.country}</p>
+              <p className=" h-auto text-2xl p-1">
+                Date:{" "}
+                {JSON.stringify(weather.location.localtime)
+                  .split(" ")[0]
+                  .split('"')}
+              </p>
+              <p className=" h-auto text-2xl p-1">
+                Last updated:{" "}
+                {JSON.stringify(weather.location.localtime)
+                  .split(" ")[1]
+                  .split('"')}
+              </p>
+            </div>
+          )}
+        </div>
+        <footer className="bottom-0 absolute hover:text-cyan-300 transition-colors duration-300 hover:cursor-pointer">
+          <a href="https://www.weatherapi.com/">Powered by weatherapi.com</a>
+        </footer>
       </div>
     </div>
   );
