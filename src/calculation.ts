e96d1ccbccda978d35b9534d4d3aa87eb6742d08
abhikatta <abhinaykatta97@gmail.com>
2024-08-29 13:19:00 +0530

    feat: now works like sand

--- src/calculation.ts
+++ src/calculation.ts
@@ -10,9 +10,27 @@ self.onmessage = function (event: MessageEvent<eventDataProps>) {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             if (grid[i][j] === 1) {
-                if (i < rows - 1 && grid[i + 1][j] === 0) {
-                    newGrid[i + 1][j] = 1;
-                    newGrid[i][j] = 0;
+                if (i < rows - 1 && j < cols) {
+                    if (grid[i + 1][j] === 0) {
+                        newGrid[i + 1][j] = 1;
+                        newGrid[i][j] = 0;
+                    } else if (
+                        i + 1 < rows &&
+                        j + 1 < cols &&
+                        grid[i + 1][j + 1] === 0
+                    ) {
+                        newGrid[i + 1][j + 1] = 1;
+                        newGrid[i][j] = 0;
+                    } else if (
+                        i + 1 < rows &&
+                        j - 1 >= 0 &&
+                        grid[i + 1][j - 1] === 0
+                    ) {
+                        newGrid[i + 1][j - 1] = 1;
+                        newGrid[i][j] = 0;
+                    } else {
+                        newGrid[i][j] = 1;
+                    }
                 } else {
                     newGrid[i][j] = 1;
                 }
